b3092427af3528bfc5563e57c2c44affe69e8100
Ula Krawczyk <ula.krawczyk7@gmail.com>
2019-11-23 13:47:16 +0100

    feat: adding images and creating base file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -19,37 +19,31 @@ const errorHandler = err => {
   })(err);
 };
 
-gulp.task("assets", function() {
+gulp.task("assets", function () {
   return gulp.src("./src/assets/**/*").pipe(gulp.dest("./dist/assets/"));
 });
 
-gulp.task("html", function() {
+gulp.task("html", function () {
   return gulp
     .src("./src/content/**/*.html")
     .pipe(plumber(errorHandler))
     .pipe(gulp.dest("./dist/"));
 });
 
-gulp.task("pwa", function() {
-  return gulp
-    .src("./src/pwa/**/*")
-    .pipe(gulp.dest("./dist/"));
-});
-
-gulp.task("js", function() {
+gulp.task("js", function () {
   return gulp.src('src/js')
-  .pipe(
-    plumber({
-      errorHandler: function(err) {
-        notify.onError({
-          title: `Gulp error in ${err.plugin}`,
-          message: err.toString()
-        })(err);
-      }
-    })
-  )
-  .pipe(webpack(require('./webpack.config.js')))
-  .pipe(gulp.dest('dist/js'));
+    .pipe(
+      plumber({
+        errorHandler: function (err) {
+          notify.onError({
+            title: `Gulp error in ${err.plugin}`,
+            message: err.toString()
+          })(err);
+        }
+      })
+    )
+    .pipe(webpack(require('./webpack.config.js')))
+    .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task("sass", () => {
@@ -57,7 +51,7 @@ gulp.task("sass", () => {
     .src("./src/scss/main.scss")
     .pipe(
       plumber({
-        errorHandler: function(err) {
+        errorHandler: function (err) {
           notify.onError({
             title: `Gulp error in ${err.plugin}`,
             message: err.toString()
@@ -70,7 +64,10 @@ gulp.task("sass", () => {
     .on("error", sass.logError)
     .pipe(
       postcss([
-        autoprefixer({ grid: true, browsers: ["> 5%", "last 4 versions"] })
+        autoprefixer({
+          grid: true,
+          browsers: ["> 5%", "last 4 versions"]
+        })
       ])
     )
     .pipe(sourcemaps.write("."))
@@ -80,19 +77,18 @@ gulp.task("sass", () => {
 
 gulp.task(
   "serve",
-  gulp.series("sass", "html", "js", "assets", "pwa", function() {
+  gulp.series("sass", "html", "js", "assets", function () {
     browserSync.init({
       server: "./dist",
       open: true // set to false to disable browser autostart
     });
     gulp.watch("src/scss/**/*", gulp.series("sass"));
     gulp.watch("src/content/**/*.html", gulp.series("html"));
-    gulp.watch("src/pwa/**/*", gulp.series("pwa"));
     gulp.watch("src/js/*.js", gulp.series("js"));
     gulp.watch("src/assets/**/*", gulp.series("assets"));
     gulp.watch("dist/**/*").on("change", browserSync.reload);
   })
 );
 
-gulp.task("build", gulp.series("sass", "html", "js", "assets", "pwa"));
+gulp.task("build", gulp.series("sass", "html", "js", "assets"));
 gulp.task("default", gulp.series("serve"));
